23591d318b14c3d5a3207330575e5e9f1a011299
hridoy <hridmamun@gmail.com>
2025-10-03 21:16:48 +0600

    create a function

--- main.go
+++ main.go
@@ -10,5 +10,15 @@ func main() {
 string
 */
 var x=10
-fmt. Println(x)
+fmt.Println(x)
+
+result := sum(10,20)
+
+fmt.Println("final result is: ", result)
+}
+
+
+func sum(x int, y int ) int {
+	var sum = x + y;
+	return sum;
 }
