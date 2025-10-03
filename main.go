a3ccffb074bb53871cede0d79b859509d7689f49
Tuhin <alauddintuhin1411@gmail.com>
2025-10-03 22:57:07 +0600

    conflict resolved for sub method

--- main.go
+++ main.go
@@ -4,11 +4,23 @@ import "fmt"
 
 func main() {
 	age	:= 20
-
 	if age>18 {
 		fmt.Println("You are eligable to married")
-
-	}else if age<18{
+	} else if age<18{
 		fmt.Println("You are not eligable to married")
 	}
+fmt.Println("final result is: ", result)
+fmt.Println("final result of subtraction is : ", sub(20,10))
+}
+
+
+func sum(x int, y int ) int {
+	var sum = x + y;
+	return sum;
+}
+
+
+func sub(x int, y int ) int {
+	var sub = x - y;
+	return sub;
 }
