4c1daaacb88bb9dc3abd5610c750a9974dd1f2a1
hridoy <hridmamun@gmail.com>
2025-10-08 23:14:05 +0600

    initial commit of go code with function definations

--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	age	:= 20
-	if age>18 {
-		fmt.Println("You are eligable to married")
-	} else if age<18{
-		fmt.Println("You are not eligable to married")
+
+const a = 10
+var p = 100
+
+func call() {
+	add:= func(x int,y int) {
+		z:=x+y
+		fmt.Println(z)
+
 	}
-fmt.Println("final result is: ", result)
-fmt.Println("final result of subtraction is : ", sub(20,10))
+	add(5,6)
+	add(p,a)
 }
 
+func main() {
+	call()
+	fmt.Println(a)
 
-func sum(x int, y int ) int {
-	var sum = x + y;
-	return sum;
 }
 
-
-func sub(x int, y int ) int {
-	var sub = x - y;
-	return sub;
+func init() {
+	fmt.Println("Hello World")
 }
+
