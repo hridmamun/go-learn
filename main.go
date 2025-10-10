b52cc751e1c9006eac2ed5f809bf2d917353ea93
hridoy <hridmamun@gmail.com>
2025-10-10 18:55:08 +0600

    It is my off day

--- main.go
+++ main.go
@@ -167,7 +167,7 @@ func main() {
 	fmt.Println("Country", country)
 }
 */
-
+/*
 func main() {
 	number:=10
 
@@ -179,6 +179,22 @@ func main() {
 		fmt.Println(number,"is zero")
 	}
 	}
-	
+	*/
+
+
+	func main() {
+		day:=3
+
+		switch day {
+		case 1:
+			fmt.Println("Monday")
+		case 2:
+			fmt.Println("Tuesday")
+		case 3:
+			fmt.Println("Wednesday")
+		default:
+			fmt.Println("Other day")
+		}
+	}
 
 
