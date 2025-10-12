1d8e549158c411064557d8c26d1158da15281396
hridoy <hridmamun@gmail.com>
2025-10-12 16:06:01 +0600

    Multiple Return Value

--- main.go
+++ main.go
@@ -269,11 +269,25 @@ func main() {
 					}
 				*/
 
-
+/*
 				func main() {
 					a:=10
 					b:=20
 					sum:=add(a,b)
 					fmt.Println("Sum:",sum)
 				}
-	
+	*/
+
+	func getNumbers(num1 int,num2 int) (int,int) {
+		sum:=num1+num2
+		mul:=num1*num2
+		return sum,mul
+	}
+
+	func main() {
+		a:=10
+		b:=20
+		p,q:=getNumbers(a,b)
+		fmt.Println("Sum:",p)
+		fmt.Println("Multiplication:",q)
+	}
