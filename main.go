a11774b2e3cc2f095d747c1d0717b66f3a2df7f8
hridoy <hridmamun@gmail.com>
2025-10-15 15:22:47 +0600

    End of internal memory

--- main.go
+++ main.go
@@ -543,7 +543,7 @@ func main() {
 					}
 						*/
 
-
+/*
 						var a = 10
 						func add(x int,y int) {
 							z:=x+y
@@ -558,3 +558,23 @@ func main() {
 						func init() {
 							fmt.Println("Hello")
 						}
+							*/
+
+							const a = 10
+							var p = 100
+							func call() {
+								add:=func(x int,y int) {
+									z:=x+y
+									fmt.Println(z)
+								}
+								add(5,6)
+								add(p,a)
+							}
+
+							func main() {
+								call()
+								fmt.Println(a)
+							}
+							func init() {
+								fmt.Println("Hello")
+							}
